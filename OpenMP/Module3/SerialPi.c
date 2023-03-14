7b2b932dae5c13c9f783c4868a25afc385dcc77d
Daniel Owen <Pilotdan747@gmail.com>
2023-03-13 19:31:31 -0500

    Add print and time
    Added functions to print pi and the process time. Added functions to track processing time

--- OpenMP/Module3/SerialPi.c
+++ OpenMP/Module3/SerialPi.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <omp.h>
 
 static long num_steps = 100000;
 double step;
 
 int main() {
     int i; double x, pi, sum = 0.0;
+
+    double t1 = omp_get_wtime();
 
     step = 1.0/(double) num_steps;
 
@@ -14,4 +17,9 @@ int main() {
     }
 
     pi = step*sum;
+
+    double t2 = omp_get_wtime();
+
+    printf("Pi is: %f\n", pi);
+    printf("It took %f seconds\n", t2 - t1);
 }
